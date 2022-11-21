711314d49307aad34c6e810c3ba133ea1e8b1104
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-21 16:26:09 +0530

    adding

--- Employee.java
+++ Employee.java
@@ -5,27 +5,69 @@ class Employee{
 
     int emp_code;
     String name;
-    String comp_name;
+    String cmp_name;
     double phone;
     String email;
+
+    Employee(int emp_code,String name,String cmp_name,double phone,String email){
+        this.emp_code=emp_code;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.cmp_name = cmp_name;
+    }
+
+    void display(){
+        System.out.println("Emp code: "+emp_code);
+        System.out.println("name: "+name);
+        System.out.println("company name: "+cmp_name);
+        System.out.println("phone: "+phone);
+        System.out.println("email: "+email);
+
+    }
 
     
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     ArrayList<Employee> emp_list = new ArrayList<Employee>();
+    int choice = 0;
+while(choice!=6){
+        System.out.println("Employee");
+        System.out.println("-------------");
+        System.out.println();
+        System.out.println("1:Add employee");
+        System.out.println("2:View employee");
+        System.out.println("3:Search employee");
+        System.out.println("4:Delete employee");
+        System.out.println("5:Exit");
 
-    System.out.println("Employee");
-    System.out.println("-------------");
-    System.out.println();
-    System.out.println("1:Add employee");
-    System.out.println("2:View employee");
-    System.out.println("3:Search employee");
-    System.out.println("4:Delete employee");
-    System.out.println("5:Exit");
+        choice=sc.nextInt();
 
-    
+        switch(choice){
+            case 1:{
+                Scanner sc1 =new Scanner(System.in);
+                System.out.println("name: ");
+                String name = sc1.nextLine();
+                System.out.println("Empcode: ");
+                int emp_code = sc1.nextInt();
+                System.out.println("Company name: ");
+                String cmp_name = sc1.nextLine();
+                System.out.println("phone: ");
+                Double phone=sc1.nextDouble();
+                System.out.println("email: ");
+                String email = sc1.nextLine();
 
+                emp_list.add(new Employee(emp_code, name, cmp_name, phone, email));
+            }
+            
+            
+
+            emp_list.get(0).display();
+        }
+
+
+    }
 
 }
 }
