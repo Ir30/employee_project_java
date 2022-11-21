4e6e2dbb7a7d6644db352339402b47ddac80ffe6
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-21 15:37:03 +0530

    starting

---
+++ Employee.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Employee{
+
+    int emp_code;
+    String name;
+    String comp_name;
+    double phone;
+    String email;
+
+    
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    ArrayList<Employee> emp_list = new ArrayList<Employee>();
+
+    System.out.println("Employee");
+    System.out.println("-------------");
+    System.out.println();
+    System.out.println("1:Add employee");
+    System.out.println("2:View employee");
+    System.out.println("3:Search employee");
+    System.out.println("4:Delete employee");
+    System.out.println("5:Exit");
+
+    
+
+
+}
+}
